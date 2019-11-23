6e58303106a6061baf15e7034a6c1eb2ea25d085
specer-f <36414185+specer-f@users.noreply.github.com>
2019-11-23 15:08:16 +0300

    Задание 2

---
+++ task2/task2.php
@@ -0,0 +1,45 @@
+<?php
+	$errors=trim($_POST['errors']);
+	$warnings=trim($_POST['warnings']);
+	if (empty($_POST['errors']) and empty($_POST['warnings'])){
+		exit('Введите значения');
+	}
+	echo "Введённые вами данные: ".$errors." и ".$warnings."<br/>";
+
+	if ($warnings == 0){
+		if ($errors % 2 == 0){
+			$commit = $errors / 2;
+		} else {
+			$commit = -1;
+		}
+	} else {
+		if ($errors % 2 == 0){
+			if ($warnings % 4 == 0){
+				$commit = $warnings / 2;
+				$errors += $warnings / 4;
+				$commit += $errors / 2;
+			} else {
+				$commit = 4 - ($warning % 4);
+				$warnings += $commit;
+				$commit += $warning / 2;
+				$errors += $warnings / 4;
+				$commit += $errors / 2;
+			}
+		} else {
+			if ($warnings % 4 == 0){
+				$commit = 2;
+				$warnings += 2;
+				$commit += $warnings / 2;
+				$errors += $warnings / 2;
+				$commit += $errors / 2;
+			} else {
+				$commit = $warnings % 4;
+				$warnings += $commit;
+				$commit += $warnings / 2;
+				$errors += $warnings / 2;
+				$commit += $errors / 2;
+			}
+		}
+	}
+	echo "Количество коммитов: ".$commit;
+?>
